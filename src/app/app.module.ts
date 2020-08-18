2933f51db611276e74f38b228ebb2b1103ea47f4
milosz bukala <milosz.bukala@princity.com>
2020-08-18 11:59:18 +0200

    create new component responsible for every new income or expense

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
 import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { BudgetItemCardComponent } from './budget-item-list/budget-item-card/budget-item-card.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AddItemFormComponent } from './add-item-form/add-item-form.component';
     MainPageComponent,
     BudgetItemListComponent,
     AddItemFormComponent,
+    BudgetItemCardComponent,
   ],
   imports: [
     BrowserModule,
