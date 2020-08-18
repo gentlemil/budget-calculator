935151c681cb60819439dabb6fb6b95bdf2290b2
milosz bukala <milosz.bukala@princity.com>
2020-08-18 13:22:46 +0200

    finish creating basic UI, create budget-item model and start making functionality for our app

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+// import { BudgetItemCardComponent } from '../budget-item-list/budget-item-card/budget-item-card.component';
+import { NgForm } from '@angular/forms';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
 
 @Component({
   selector: 'app-add-item-form',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddItemFormComponent implements OnInit {
 
+  @Input() item: BudgetItem = new BudgetItem('', null)
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 }
---
+++ src/shared/models/budget-item.model.ts
@@ -0,0 +1,12 @@
+export class BudgetItem {
+    // description: string;
+    // amount: number;
+
+    constructor(public description: string, public amount: number) {
+        // no need to write two lines below if we made description and amount public
+        // good bless typescript
+
+        // this.description = description;
+        // this.amount = amount;
+    }
+}
